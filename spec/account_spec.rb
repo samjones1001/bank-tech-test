9809abcce469ff3cccd1eea3a2719ec1e9e74ddf
Sam Jones <sam.jones494@googlemail.com>
2017-03-09 11:36:36 +0000

    account starts with a balance of 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,4 +2,13 @@ require_relative '../lib/account'
 
 describe Account do
 
+  let(:account)   { described_class.new }
+
+  it 'can create a new account' do
+    expect{ Account.new }.not_to raise_error
+  end
+
+  it 'begins with a balance of 0' do
+    expect(account.balance).to eq 0
+  end
 end
